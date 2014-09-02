7e3d6093e92a9b497cf14eea0ab0a9f8cf72028b
Jesse <Jesse@128.61.24.3>
2014-09-02 19:18:17 -0400

    Test

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -5,20 +5,20 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *
- * @author Bob
+ * @author Jesse Chen
  * @version 1.1
  */
 public class Person2 {
     /** Holds the persons real name */
     private String name;
-	 	/**
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-	 public Person2(String pname) {
-	   name = pname;
-	 }
+	public Person2(String pname) {
+            name = pname;
+        }
 	/**
 	 * This method should take the string
 	 * input and return its characters in
